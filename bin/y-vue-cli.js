48761fcbf388dc6d896cd57318230df3ace8c293
赵雅国 <1248023357@qq.com>
2022-12-21 23:27:17 +0800

    feat: y-cli function ok

--- bin/y-vue-cli.js
+++ bin/y-vue-cli.js
@@ -26,41 +26,56 @@ function initHelp () {
   if (program.args.length < 1) return program.help()
 }
 
-initHelp()
 
-/**
- * 获取命令行参数
- */
-const templateName = program.args[0] // 命令行第一个参数 文件夹名字 template
-console.log(program.args, '-----')
-/**
- * 获取项目和模版的完整路径
-*/
-const targetPath = path.join(process.cwd(), templateName) // 模版的路径  cwd是当前运行的脚本是在哪个路径下运行 tem
-console.log(targetPath)
 
-// if (exists.existsSync(targetPath)) {
-//   console.log(chalk.red('  # The same project name already exists in the current directory.'))
-// } else {
-  run()
-// }
+
+function start() {
+
+  initHelp()
+  /**
+   * 获取命令行参数
+   */
+  const templateName = program.args[0] // 命令行第一个参数 文件夹名字 template
+  console.log(program.args, '-----')
+  /**
+   * 获取项目和模版的完整路径
+  */
+  const targetPath = path.join(process.cwd(), templateName) // 模版的路径  cwd是当前运行的脚本是在哪个路径下运行 tem
+  console.log(targetPath)
+
+  if (exists.existsSync(targetPath)) {
+    console.log(chalk.red('  # The same project name already exists in the current directory.'))
+  } else {
+    exists.mkdirSync(targetPath)
+    run(templateName, targetPath)
+  }
+}
 
 /**
  * run函数则是用来调用generate来构建项目
  */
-function run() {
+function run(templateName, targetPath) {
   const spinner = ora({
     text: `${chalk.green('downloading template')}`,
   })
-  // spinner.start()
+  spinner.start()
   // TODO 这里可以先获取用户输入，然后下载文件的时候过滤去填充
-  generate(templateName, targetPath, (err) => { // 构建完成的回调函数
-    console.log('构建完成')
-    if (err) console.log(err) // 如果构建失败就输出失败原因
+  download(`GGupzHH/Vue3-Vite3-TS-Template#y-cli-template`, targetPath, {}, err => {
+    spinner.stop()
+    if (err) return
+    // console.log(chalk.green(`模版下载完成 ${ targetPath }`))
+    generate(templateName, targetPath, (err) => { // 构建完成的回调函数
+      console.log('构建完成')
+      console.log(`🎉  Successfully created project ${templateName}.`)
+      console.log(`👉  Get started with the following commands:`)
+      console.log(` $ cd ${templateName}`)
+      console.log(` $ yarn serve`)
+      
+      if (err) console.log(err) // 如果构建失败就输出失败原因
+    })
   })
-  // download(`tccsg/next-template`, targetPath, {}, err => {
-  //   spinner.stop()
-  //   if (err) return
-  //   console.log(chalk.green(`模版下载完成 ${ targetPath }`))
-  // })
 }
+
+
+
+start()
